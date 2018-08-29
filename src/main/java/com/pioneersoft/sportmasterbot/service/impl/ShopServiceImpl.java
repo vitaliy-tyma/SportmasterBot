71f5adf55a3a9c90dd47e163476d8f1277362f41
Alex <ignatenko2207@gmail.com>
2018-08-30 00:14:41 +0300

    Make shop service

--- src/main/java/com/pioneersoft/sportmasterbot/service/impl/ShopServiceImpl.java
+++ src/main/java/com/pioneersoft/sportmasterbot/service/impl/ShopServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.Map;
 @Service
 public class ShopServiceImpl implements ShopService {
 
-
+    //400767WB38
     @Override
     public Map<String, Shop> getAllShops() {
         Map<String, Shop> allShops = new HashMap<>();
@@ -28,23 +28,55 @@ public class ShopServiceImpl implements ShopService {
                 DocumentContext context = JsonPath.parse(content);
                 List<Map<String, Object>> shops = context.read("$.*");
 
-                for ( Map<String, Object> shop : shops ){
+                for (Map<String, Object> shop : shops) {
+                    Shop shopBO = new Shop();
+                    shopBO.setShopId(String.valueOf(shop.get("id")));
+                    shopBO.setAddress(String.valueOf(shop.get("address")));
+                    if (shop.containsKey("metro")) {
+                        shopBO.setShopId(String.valueOf(shop.get("metro")));
+                    } else {
+                        shopBO.setMetroStation("");
+                    }
 
+                    allShops.put(shopBO.getShopId(), shopBO);
                 }
-
             }
-
-
         } catch (IOException e) {
             LogManager.writeLogText(e.getMessage());
         }
-
-
-        return null;
+        return allShops;
     }
 
     @Override
     public Map<String, Shop> filterShopsByAvailableItem(Map<String, Shop> allShops, String itemId) {
-        return null;
+        Map<String, Shop> filteredShops = new HashMap<>();
+
+        if ( allShops.isEmpty()){
+            return filteredShops;
+        }
+
+        try {
+            String content = Jsoup.connect("https://www.sportmaster.ru/rest/v1/sku/" + itemId + "/store/region/stock/core?_=" + System.currentTimeMillis()).get().text();
+
+            if (content != null && content.startsWith("{")) {
+                DocumentContext context = JsonPath.parse(content);
+                List<Map<String, Object>> filters = context.read("$.*");
+
+
+
+                for (Map<String, Object> filter : filters) {
+
+                    if (filter.containsKey("stockQuantity")){
+                        Map<String, Object> stockQuantity = (Map<String, Object>)filter.get("stockQuantity");
+                        if (stockQuantity.containsKey("inStoreIndicator") && !(String.valueOf(stockQuantity.get("inStoreIndicator")).equalsIgnoreCase("NONE"))){
+                            filteredShops.put(String.valueOf(filter.get("storeId")), allShops.get(String.valueOf(filter.get("storeId"))));
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            LogManager.writeLogText(e.getMessage());
+        }
+        return filteredShops;
     }
 }
