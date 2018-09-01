b34e39c17fb2f5cdfb7d3d9fa1ef7244e5734159
Alex <ignatenko2207@gmail.com>
2018-09-01 15:32:29 +0300

    Start with controllers

---
+++ src/main/java/com/pioneersoft/sportmasterbot/controller/OrderController.java
@@ -0,0 +1,10 @@
+package com.pioneersoft.sportmasterbot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/order")
+public class OrderController {
+
+}
---
+++ src/main/java/com/pioneersoft/sportmasterbot/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.pioneersoft.sportmasterbot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/user")
+public class UserController {
+
+
+}
