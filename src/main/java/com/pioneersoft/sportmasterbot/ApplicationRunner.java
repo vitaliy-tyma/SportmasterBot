aeb8e2c4777db93f66d7727d9d97aeed52eb710b
Alex <ignatenko2207@gmail.com>
2018-09-16 22:48:05 +0300

    Made war. Path to files must be fixed.

--- src/main/java/com/pioneersoft/sportmasterbot/ApplicationRunner.java
+++ src/main/java/com/pioneersoft/sportmasterbot/ApplicationRunner.java
@@ -6,12 +6,18 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class ApplicationRunner
-{
+public class ApplicationRunner extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationRunner.class);
+    }
+
     public static void main( String[] args )
     {
         LogManager.writeLogText("-------------------- Start application --------------------");
