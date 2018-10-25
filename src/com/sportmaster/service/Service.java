37a901afe843a35e9ac88260c77c17843e70ece7
vitaliy-tyma <vitaliy-tyma@DESKTOP-OBMD4J8>
2018-10-25 20:04:02 +0300

    Under construction

--- src/com/sportmaster/service/Service.java
+++ src/com/sportmaster/service/Service.java
@@ -106,10 +106,6 @@ public class Service {
 			WebElement itemUrlElement1 = itemUrlElement.findElement(By.tagName("a"));
 			String itemUrl = itemUrlElement1.getAttribute("href");
 
-//			NOT WORK!!! 
-//			WebElement itemUrl1 = itemUrlElement.findElement(By.cssSelector("a href"));
-//			logger.log(Level.INFO,"!!!!!!!!!!!!!!!! itemUrl1 = " + itemUrl1.getText());
-
 			logger.log(Level.INFO, "**************** itemUrl = " + itemUrl);
 			driver.get(itemUrl);
 
@@ -145,7 +141,7 @@ public class Service {
 			 */
 
 			good.setGoodID(goodID);
-			
+
 			logger.log(Level.INFO, "Good Name (POJO) has been set = " + goodName);
 			logger.log(Level.INFO, "Good Price (POJO) has been set = " + goodPrice);
 			logger.log(Level.INFO, "Good ID (POJO) has been set = " + goodID);
@@ -155,119 +151,141 @@ public class Service {
 		Timer.delay(3);
 		return driver;
 	}
-	
+
 	////////////////////////////////////////////////////////////////////////
 	public static WebDriver pressBuyButton(WebDriver driver, String goodID, Good good) {
 		try {
-			
-			//.Service.css=.sm-goods_main_details_buy-straight
+
+			// .Service.css=.sm-goods_main_details_buy-straight
 			WebElement itemBuyElement = driver.findElement(By.className("sm-goods_main_details_buttons"));
 			WebElement itemBuy = itemBuyElement.findElement(By.className("sm-goods_main_details_buy-straight"));
 			itemBuy.click();
 			logger.log(Level.INFO, "Buy Button has been pressed.");
 
-			//div id="basketMessage"
-			//data-selenium="go_to_basket"
+			// div id="basketMessage"
+			// data-selenium="go_to_basket"
 			WebElement itemUrlElement = driver.findElement(By.id("basketMessage"));
 			WebElement itemUrlElement1 = itemUrlElement.findElement(By.cssSelector("a[data-selenium='go_to_basket'"));
 			String itemUrl = itemUrlElement1.getAttribute("href");
 			logger.log(Level.INFO, "itemUrl1 = " + itemUrl);
 
-			//Press go to basket
+			// Press go to basket
 			logger.log(Level.INFO, "Go to basket has been pressed.");
 			driver.get(itemUrl);
-			
+
 			//
-			//WHAT TO DO NEXT??????
+			// WHAT TO DO NEXT??????
 			//
-			
-			
+
 		} catch (NoSuchElementException e) {
 			logger.log(Level.SEVERE, "**********No Buy Button has been found while buying procedure.");
 		}
 		Timer.delay(3);
 		return driver;
 	}
-	
-	
+
 	////////////////////////////////////////////////////////////////////////
-	//GET FIRST SHOP WHERE GOOD IS PRESENT
-	public static Shop clarifyGoodPresence(WebDriver driver, Good good) throws NoShopsWithGoodException{
+	// GET FIRST SHOP WHERE GOOD IS PRESENT
+	public static Shop clarifyGoodPresence(WebDriver driver, Good good) throws NoShopsWithGoodException {
 		Shop shopFirst = new Shop();
 		try {
 
-			//class="sm-goods_tabs_block"
-			//li data-selenium="product_tab"
+			// class="sm-goods_tabs_block"
+			// li data-selenium="product_tab"
 			WebElement itemClarifyElement = driver.findElement(By.className("sm-goods_tabs_block"));
-			List <WebElement> itemClarifyList = itemClarifyElement.findElements(By.cssSelector("li[data-selenium='product_tab'"));
-			
+			List<WebElement> itemClarifyList = itemClarifyElement
+					.findElements(By.cssSelector("li[data-selenium='product_tab'"));
+
 			for (WebElement element : itemClarifyList) {
 				if (element.getAttribute("data-bind").contains("availability")) {
 					element.click();
 					logger.log(Level.INFO, "Check availability Button has been pressed.");
 					break;
-					}
+				}
 			}
+			Timer.delay(10);
 
-
-			
-			//class="sm-goods_tabs_presence-availability clearfix"
-			//value="available"
+			// class="sm-goods_tabs_presence-availability clearfix"
+			// value="available"
 			WebElement itemTabsPesence = driver.findElement(By.className("sm-goods_tabs_presence-availability"));
-			List <WebElement> itemTabsList = itemTabsPesence.findElements(By.cssSelector("input"));
-			
-			for (WebElement element : itemTabsList ) {
+			List<WebElement> itemTabsList = itemTabsPesence.findElements(By.cssSelector("input"));
+
+			for (WebElement element : itemTabsList) {
 				if (element.getAttribute("value").contains("available")) {
 					element.click();
 					logger.log(Level.INFO, "Available radio Button has been pressed.");
 					break;
-					}
+				}
 			}
-			
-			
-			
-			
-			
-			//table
-			//class="ccrests__table"
+			Timer.delay(5);
+
+			// table
+			// class="ccrests__table"
 			WebElement itemTableElement = driver.findElement(By.className("ccrests__table"));
-			List <WebElement> itemTableRowsList = itemTableElement.findElements(By.cssSelector("tr"));
-			//
-			//Check if list is not empty - otherwise fail with the shop!!!!
+			// logger.log(Level.INFO, "!!!! itemTableElement = " +
+			// itemTableElement.getAttribute("innerHTML"));
+
+			List<WebElement> itemTableRowsList = itemTableElement.findElements(By.cssSelector("tr"));
+
+			// Check if list is not empty - otherwise fail with the shop!!!!
 			if (itemTableRowsList.size() == 0) {
 				throw new NoShopsWithGoodException();
 			}
 			
-			
-			for (WebElement element : itemTableRowsList ) {
-				//class="ccrests__table__column-address"
-				//a
-				//class="ccrests__store-address"
-				
-				
-				//class="ccrests__table__column-metro ccrests__store-metro"
-				//span
-				/*WebElement shopAddressElement = element.findElement(By.className("ccrests__store-address"));
+			/**
+			 * FURTHER NOT WORK!!! PLEASE STOP HERE!!!
+			 */
+
+			Timer.delay(5);
+			for (WebElement element : itemTableRowsList) {
+				List<WebElement> itemTDList = element.findElements(By.tagName("td"));
+				logger.log(Level.INFO, "!!!! element innerHTML = " + element.getAttribute("innerHTML"));
+
+				logger.log(Level.INFO, "!!!! element outerHTML = " + element.getAttribute("outerHTML"));
+
+				for (WebElement el : itemTDList) {
+
+					logger.log(Level.INFO, "!!!! element = " + el.getAttribute("innerHTML"));
+					logger.log(Level.INFO, "!!!! element = " + el.getAttribute("innerHTML"));
+
+					WebElement shopAddressElement = el.findElement(By.className("ccrests__table__column-address"));
+					WebElement shopAddressElement1 = el.findElement(By.className("ccrests__store-address"));
+
+					logger.log(Level.INFO, "Element2 = " + shopAddressElement.toString());
+					logger.log(Level.INFO, "Element2 = " + shopAddressElement1.getText());
+
+				}
+				// class="ccrests__table__column-address"
+				// a
+				// class="ccrests__store-address"
+
+				// class="ccrests__table__column-metro ccrests__store-metro"
+				// span
 				WebElement shopMetroElement = element.findElement(By.className("ccrests__table__column-metro"));
-				
-				shopFirst.setShopAddress(shopAddressElement.getText());
-				shopFirst.setShopMetro(shopMetroElement.getText());
-				shopFirst.setShopID("&&&&&ID");
-				*/
-				logger.log(Level.INFO, "----------------Shop First has been created.");
+				logger.log(Level.INFO,
+						"!!!! shopMetroElement innerHTML = " + shopMetroElement.getAttribute("innerHTML"));
+				logger.log(Level.INFO,
+						"!!!! shopMetroElement outerHTML = " + shopMetroElement.getAttribute("outerHTML"));
+
+				WebElement shopMetro = shopMetroElement.findElement(By.cssSelector("span"));
+
+				// WebElement shopAddressElement1 =
+				// shopAddressElement.findElement(By.cssSelector("a"));
+
+				// shopFirst.setShopAddress(shopAddressElement1.getText());
+				shopFirst.setShopMetro(shopMetro.getText());
+				////////////////////////////////////////////////////////////////
+				shopFirst.setShopID("&&&&&ID");/////////////////////////////////
+
+				logger.log(Level.INFO, "----------------Shop (First in row) has been created.");
 			}
-			
-			
-			
-			
-			
+
 		} catch (NoSuchElementException e) {
 			logger.log(Level.SEVERE, "**********No Shop has been found while buying procedure.");
 		}
 		Timer.delay(3);
 		return shopFirst;
 	}
-	
 
 	// TO BE DELETED!!!!
 	// logger.log(Level.INFO, "input_name_element = " +
